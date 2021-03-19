9c6762b4ca61d8628c6daf1bbacf06b24126e5ff
Preston <childrep@vt.edu>
2021-03-18 22:45:24 -0400

    Styling for CAD Viewer

--- src/CadViewer.js
+++ src/CadViewer.js
@@ -1,27 +1,19 @@
 import React, { useContext } from "react";
 import "@google/model-viewer";
-import { makeStyles } from "@material-ui/core/styles";
 import AppContext from "./AppContext";
 
-const useStyles = makeStyles((theme) => ({
-  sizing: {
-    width: "100%",
-    minWidth: "300px",
-  },
-}));
-
 export default function RobotSlides() {
-  const classes = useStyles();
   const cadContext = useContext(AppContext);
   return (
     <model-viewer
-      className={classes.sizing}
       autoplay
       auto-rotate
       camera-controls
       exposure="0.25"
+      interaction-prompt="none"
       shadow-intensity="1"
       src="./Assembly 1.gltf"
+      style={{ width: "100%", height: "100%" }}
     ></model-viewer>
   );
 }
